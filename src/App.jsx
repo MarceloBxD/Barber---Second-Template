6b2b8de54524175d56f3dc13494d02bbe3d32167
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-05-10 19:45:36 -0300

    last commit, changing the title img

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react";
 import "./App.css";
 import { GlobalStyles } from "./GlobalStyles";
 import { Header } from "./components";
 
 function App() {
+  // setar o titulo da pagina com slice
+  let title = document.title;
+
+  // motrar o title letra por letra com settimout
+  useEffect(() => {
+    let i = 0;
+    let titleLength = title.length;
+    let titleInterval = setInterval(() => {
+      if (i <= titleLength) {
+        document.title = title.slice(0, i);
+        i++;
+      } else {
+        i = 0;
+      }
+    }, 100);
+    return () => {
+      clearInterval(titleInterval);
+    };
+  }, [title]);
+
   return (
     <div>
       <Header />
